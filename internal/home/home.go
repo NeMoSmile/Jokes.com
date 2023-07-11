2bf4750153ff8a42d4d12072ed3a84ea10e32e6e
NeMoSmile <samirtihonov@gmail.com>
2023-07-11 22:11:38 +0300

    k

--- internal/home/home.go
+++ internal/home/home.go
@@ -6,7 +6,7 @@ import (
 )
 
 func MainHandler(w http.ResponseWriter, r *http.Request) {
-	cookie, err := r.Cookie("username")
+	_, err := r.Cookie("username")
 	if err != nil {
 		http.Redirect(w, r, "/", http.StatusFound)
 		return
@@ -15,8 +15,5 @@ func MainHandler(w http.ResponseWriter, r *http.Request) {
 	err = tmpl.Execute(w, nil)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-	if cookie.Value != "yo" {
-		http.Redirect(w, r, "/what", http.StatusFound)
 	}
 }
